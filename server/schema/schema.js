5bb32ef1c4fbc03d526d72f79b4cdbac1d311b32
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-29 14:59:49 -0500

    Created initial schema for users

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,41 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+} = graphql;
 
 // Create types
-const userType = new GraphQLObjectType({
+const UserType = new GraphQLObjectType({
   name: "User",
   description: "Documentation for user",
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
   }),
+});
+
+// RootQuery - path that allows up to traverse through query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  description: "Description",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+
+      resolve(parent, arg) {
+        //we resolve with data
+        //get and return data from a dataSource
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
