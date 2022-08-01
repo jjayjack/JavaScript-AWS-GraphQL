aa190c34a1db7f989a9ce62aa2cec9bde4012a88
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-01 16:18:50 -0500

    Removal of hard-coded data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -13,53 +13,6 @@ const {
 	GraphQLList
 } = graphql;
 
-var usersData = [
-	{ id: "1", name: "Dave", age: 28, profession: "Programmer" },
-	{ id: "13", name: "Anna", age: 32, profession: "Scrum Master" },
-	{ id: "211", name: "Bella", age: 36, profession: "Practice Manager" },
-	{ id: "19", name: "Gina", age: 29, profession: "Human Resources" },
-	{ id: "150", name: "George", age: 26, profession: "Programmer" }
-];
-var hobbiesData = [
-	{
-		id: "1",
-		title: "Programming",
-		description: "Using computers to create cool things",
-		userId: "19"
-	},
-	{
-		id: "2",
-		title: "Cooking",
-		description: "Stoves and grills are cool",
-		userId: "211"
-	},
-	{
-		id: "3",
-		title: "Swimming",
-		description: "Water is cool",
-		userId: "13"
-	},
-	{
-		id: "4",
-		title: "Hiking",
-		description: "Nature is cool",
-		userId: "1"
-	},
-	{
-		id: "1",
-		title: "Playing with dogs",
-		description: "Dogs are cool",
-		userId: "19"
-	}
-];
-
-var postsData = [
-	{ id: "1", comment: "This is great", userId: "1" },
-	{ id: "12", comment: "Awesome", userId: "211" },
-	{ id: "150", comment: "WOWOWOW", userId: "1" },
-	{ id: "64", comment: "My trip at the lake!", userId: "19" }
-];
-
 const UserType = new GraphQLObjectType({
 	name: "User",
 	description: "Documentation for user",
