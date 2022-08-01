abdfdbe52af6af40cf164c13d7c9416200ee0bd9
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-01 13:41:26 -0500

    Linking app to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+
 const schema = require("./schema/schema");
 const testSchema = require("./schema/types.schema");
 
 const app = express();
-
+const port = process.env.PORT || 4000;
 app.use(
 	"/graphql",
 	graphqlHTTP({
@@ -13,6 +15,13 @@ app.use(
 	})
 );
 
-app.listen(4000, () => {
-	console.log("listening for requests");
-});
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.mongoUserName}:<${process.env.mongoUserPassword}>@clustergraphql.ffvmskm.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`,
+		{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+	)
+	.then(() => {
+		app.listen({ port: port }, () => {
+			console.log("listening for requests on " + port);
+		});
+	});
